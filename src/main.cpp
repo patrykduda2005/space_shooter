60c544f6628dc8531415f0baf820f1cd719d1fcf
rodzeer <marcelbrukalo@gmail.com>
2025-12-30 22:26:52 +0100

    Simple escape menu

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
 
+bool Pause = false;
+
 int main ()
 {
 	SetTargetFPS(144);
@@ -31,6 +33,10 @@ int main ()
 	initBulletTexture();
 	initAmmoCounter();
 
+	Rectangle resumeBtn = {450, 200, 200, 50};	
+
+	SetExitKey(KEY_NULL);
+
 	SearchAndSetResourceDir("resources");
 
 	int type = 1;
@@ -41,7 +47,7 @@ int main ()
     rabbit->add_component<Position>({.x = 500, .y = 700});
     rabbit->add_component<ArrowMovement>({200, 400, 200, 400});
     rabbit->add_component<RestrictToWorld>({});	
-	rabbit->add_component<Shooting>({.cooldown = 0.0});
+	 rabbit->add_component<Shooting>({.cooldown = 0.0});
 
     Entity* enemy = entities->new_entity();
     enemy->add_component<Position>({.x = 500, .y = 100});
@@ -50,38 +56,56 @@ int main ()
 	// game loop
 	
 
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-        float d = GetFrameTime();
+	while (!WindowShouldClose()) {		// run the loop untill the user presses ESCAPE or presses the Close button on the window 
+      
+		float d = GetFrameTime();
+		Vector2 mousePosition = GetMousePosition();
+		  
+		if(IsKeyPressed(KEY_ESCAPE)) Pause = !Pause;
+
+
 		// drawing
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(WHITE);
 
-		DrawText("Ammo", 875,650,25,BLACK);
-		DrawText("Press 1, 2 or 3 to change shooting type", 50,30,25,BLACK);
-		//DrawText(std::to_string(type).c_str(), 50,75,25,BLACK);
+		if (!Pause){
+			DrawText("Ammo", 875,650,25,BLACK);
+			DrawText("Press 1, 2 or 3 to change shooting type", 50,30,25,BLACK);
+			//DrawText(std::to_string(type).c_str(), 50,75,25,BLACK);
 
-		if (IsKeyPressed(KEY_ONE)) type = 1;
-		if (IsKeyPressed(KEY_TWO)) type = 2;
-		if (IsKeyPressed(KEY_THREE)) type = 3;
+			if (IsKeyPressed(KEY_ONE)) type = 1;
+			if (IsKeyPressed(KEY_TWO)) type = 2;
+			if (IsKeyPressed(KEY_THREE)) type = 3;
+			
+			
+			DrawText("FPS:", 50,700,25,BLACK);
+			DrawText(std::to_string(GetFPS()).c_str(), 120,700,25,BLACK); 
+
+			shoot(type);
+		   updateGravity(d);
+			updateVelocity(d);
+	      renderThings(d);
+   	   arrowMovement(d);
+  		   restrictToWorld(d);
+   	   destroyBeyondWorld();
+   	 	detectCollision();
+     		outlineColliders();
+			ammoCounter(type);
+		} 
+		else {
+			DrawText("PAUSED", 400,100,50,RED);
+			DrawText("Resume", 450, 200, 25, RED);
+			if (CheckCollisionPointRec(mousePosition, resumeBtn)) {
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                Pause = false; // Wracamy do gry
+            }
+        }
 		
 		
-		DrawText("FPS:", 50,700,25,BLACK);
-		DrawText(std::to_string(GetFPS()).c_str(), 120,700,25,BLACK); 
-
-		shoot(type);
-	   updateGravity(d);
-		updateVelocity(d);
-      renderThings(d);
-      arrowMovement(d);
-      restrictToWorld(d);
-      destroyBeyondWorld();
-    	detectCollision();
-      outlineColliders();
-		ammoCounter(type);
-
+		}
+		
         //std::cout << "Entities: " << entities->get().size() << "\n";
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
